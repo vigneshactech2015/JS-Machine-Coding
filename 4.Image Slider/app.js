bbc4d4eada1f68a632902ca24c38cb90c56dcdc5
Vignesh <85957001+vigneshactech2015@users.noreply.github.com>
2023-10-14 16:59:10 +0530

    my method

--- 4.Image Slider/app.js
+++ 4.Image Slider/app.js
@@ -21,3 +21,40 @@ btns.forEach((button)=>{
         }
     })
 })
+
+//my method 
+//method 2
+const imageList = ["https://tse2.mm.bing.net/th?id=OIP.4z2feTp_Mw25d-lmn1wMdQHaJI&pid=Api&P=0&h=180","https://tse3.mm.bing.net/th?id=OIP.ZBI4gBIQ_Dk1FoUCLlsa6AHaE1&pid=Api&P=0&h=180","https://tse4.mm.bing.net/th?id=OIP.dYWvYkXgj1jP66ZZ6PzY0QHaGc&pid=Api&P=0&h=180","https://tse2.mm.bing.net/th?id=OIP.7stwxZm-L1BqyYwVuRKYNgHaE7&pid=Api&P=0&h=180","https://tse2.mm.bing.net/th?id=OIP.EyWlvOAeyEIojrKwUmm-fAHaGl&pid=Api&P=0&h=180"]
+let index = 0;
+
+const loadImage = () => {
+    console.log(index)
+    let createContainer = document.createElement('div')
+    createContainer.className = "container"
+    createContainer.innerHTML = `<img class="styleImage" src=${imageList[index]} alt="image"/>
+    <button class="prev" onclick="prevHandler()">Prev</button>
+    <button class="next" onclick="nextHandler()">Next</button>
+    `
+    document.body.append(createContainer)
+    
+}
+
+loadImage()
+
+function prevHandler(){
+    index--
+    if(index<0){
+        index = (imageList.length-1)
+    }
+   loadImage()
+    document.querySelector('.container').remove()    
+}
+
+function nextHandler(){
+    index++
+    if(index == imageList.length-1){
+        index = 0
+    }
+   loadImage()
+   document.querySelector('.container').remove()
+}
