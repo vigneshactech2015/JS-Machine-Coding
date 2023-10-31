6f248174297cebc73ca3868cb27380e3f05edb05
Vignesh <85957001+vigneshactech2015@users.noreply.github.com>
2023-10-31 18:37:42 +0530

    Update currying.js

--- currying.js
+++ currying.js
@@ -53,6 +53,7 @@ updateHeader("Hello World Updated")
 
 function curry(func){
   return function curriedFunc(...args){
+    //base case
     if(args.length >= func.length){
         return func(...args)  
     } else {
